1345e3d7e98193d2623e73a93509e9eb8cd21cad
brunobgf <brunobgf40@gmail.com>
2024-03-02 10:50:14 -0300

    feat: trivial project

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Aluno: Bruno Gomes Ferreira \n");
+        System.out.println("Disciplina: Engenharia Econômica");
+
+        // Random comment
+        // Random comment
+        // Random comment
+        /*
+         * Another comment
+         */
+
+    }
+}
